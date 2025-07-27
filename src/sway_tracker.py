75f24e461eceb0ecfe858a70df4b73cbaf7c774a
Félix Dorn <git@xfe.li>
2025-07-28 01:29:54 +0200

    Switch to GPLv3 and add license headers

--- src/sway_tracker.py
+++ src/sway_tracker.py
@@ -1,3 +1,17 @@
+# This file is part of time-narrator.
+#
+# time-narrator is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# time-narrator is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with time-narrator.  If not, see <https://www.gnu.org/licenses/>.
 import i3ipc
 
 # A persistent connection object to be used by the application.
